3fd01b8f1895742ca24ec3028fb40fca6692a850
IbrahimS666 <anwaribrahim0873@gmail.com>
2025-07-29 10:22:41 +0530

    Add files via upload

---
+++ python.01.py
@@ -0,0 +1,14 @@
+print("takshashila university")
+print("ongur tindivanam,viluppuram district.tn")
+print("---------------------------------------")
+print("STUDENT MARK LIST")
+print("-----------------")
+str1=input("enter the register no")
+str2=input("enter the name:")
+m1=int(input("python mark"))
+m2=int(input("dbms mark"))
+m3=int(input("accounting mark"))
+total=m1+m2+m3
+print("total mark=",total)
+avg=total/3
+print("average mark",avg)      
