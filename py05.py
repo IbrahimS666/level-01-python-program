f160a29d0aa8af974a5ec15ba96193152949a133
IbrahimS666 <anwaribrahim0873@gmail.com>
2025-07-30 16:07:22 +0530

    Add files via upload

---
+++ py05.py
@@ -0,0 +1,18 @@
+print("ibrahim international pvt ltd")
+print("no ,07 acharapakkam")
+print("----------------------------")
+print("employee payroll system")
+print("----------------------")
+input("enter the employee id:")
+input("enter the employee name:")
+salary=int(input("enter the salary:"))
+print("income")
+bonus=salary*20/100
+print("bonus(20 per):",bonus)
+overtime=salary*10/100
+print("overtime(10 per):",overtime)
+travel=salary*5/100
+print("travel allowance(5 per)",travel)
+grasspay=bonus+overtime+travel
+print("grass pay in rupees:",grasspay)
+      
